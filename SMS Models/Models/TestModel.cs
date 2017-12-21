d8fb920a83e6584af47d34aa47e64f74e7aa7b77
areednehvi <areednazir@gmail.om>
2017-12-21 22:23:10 +0530

    Included Charts
    Included Charts

---
+++ SMS Models/Models/TestModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace SMS_Models.Models
+{
+    public class TestModel : BindableBase
+    {
+        private List<Keyvalue> _DataList;
+        public List<Keyvalue> DataList { get { return _DataList; } set { SetProperty(ref _DataList, value); } }
+    }
+    public class Keyvalue : BindableBase
+    {
+        private string _Key;
+        public string Key { get { return _Key; } set { SetProperty(ref _Key, value); } }
+
+        private int _Value;
+        public int Value { get { return _Value; } set { SetProperty(ref _Value, value); } }
+    }
+}
---
+++ SMS/Controllers/TestViewModel.cs
@@ -0,0 +1,38 @@
+﻿using Prism.Mvvm;
+using SMS_Models.Models;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Controls.DataVisualization.Charting;
+
+namespace SMS.Controllers
+{
+    class TestViewModel : BindableBase
+    {
+        private TestModel testModel = new TestModel();
+        public TestViewModel(GroupBox GroupBoxDynamicChart)
+        {
+            List<Keyvalue> tempList = new List<Keyvalue>();
+            tempList.Add(new Keyvalue() { Key = "jan", Value = 50 });
+            tempList.Add(new Keyvalue() { Key = "feb", Value = 18 });
+            tempList.Add(new Keyvalue() { Key = "march", Value = 25 });
+            tempList.Add(new Keyvalue() { Key = "april", Value = 5 });
+
+            //Bind the staic Chart                     
+            TestModel.DataList = tempList;
+            //Create the Dynamic Chart, Bind it and add it into the 2nd GroupBox
+            Chart dynamicChart = new Chart() { Height = 300, Width = 400 };
+            BarSeries pieSeries = new BarSeries();
+            pieSeries.ItemsSource = tempList;
+            pieSeries.DependentValuePath = "Value";
+            pieSeries.IndependentValuePath = "Key";
+            dynamicChart.Series.Add(pieSeries);
+            GroupBoxDynamicChart.Content = dynamicChart;
+        }
+        public TestModel TestModel
+        {
+            get { return testModel; }
+            set { SetProperty(ref testModel, value); }
+        }
+
+    }
+}
